9502cc303c6be592b59dbeb5aa4e0ce14c55f2c6
Mistry Pavan Kumar <68140538+mg143pavankumar@users.noreply.github.com>
2021-08-10 23:41:02 +0530

    fixed syntax errors

--- EXPERIMENT 1 - Scheduling algorithms (Non preemptive)/1.4.Roundrobin_Scheduling_algorithm.c
+++ EXPERIMENT 1 - Scheduling algorithms (Non preemptive)/1.4.Roundrobin_Scheduling_algorithm.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-main()
+void main()
 {
 int i,j,n,bu[10],wa[10],tat[10],t,ct[10],max;
 float awt=0,att=0,temp=0;
-clrscr();
+
 printf("Enter the no of processes -- ");
 scanf("%d",&n);
 for(i=0;i<n;i++)
